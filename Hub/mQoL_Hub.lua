dbe72a64c6e932767ea2a59c9c77258187b801b5
Mentiuszen <steedfastsky1337@gmail.com>
2026-02-19 14:34:29 +0100

    Change ver to 1.1.1

--- Hub/mQoL_Hub.lua
+++ Hub/mQoL_Hub.lua
@@ -11,8 +11,8 @@ mQoL_Hub.VersionData = mQoL_Hub.VersionData or {}
 
 -- Addon Version
 mQoL_Hub.version = "1.1.1"
-mQoL_Hub.build = "188"
-mQoL_Hub.vendor = "dev"	--dev / test / release
+mQoL_Hub.build = "190"
+mQoL_Hub.vendor = "test"	--dev / test / release
 
 -- Styles
 local CreateCustomScrollbar = mQoL_Styles.CreateCustomScrollbar
