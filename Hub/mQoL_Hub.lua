8c521f37ad2cadae94145c4870257d8757d07fe5
Mentiuszen <steedfastsky1337@gmail.com>
2026-02-26 22:24:14 +0100

    update build

--- Hub/mQoL_Hub.lua
+++ Hub/mQoL_Hub.lua
@@ -11,7 +11,7 @@ mQoL_Hub.VersionData = mQoL_Hub.VersionData or {}
 
 -- Addon Version
 mQoL_Hub.version = "1.2.0"
-mQoL_Hub.build = "203"
+mQoL_Hub.build = "205"
 mQoL_Hub.vendor = "dev"	--dev / test / release
 
 -- Styles
